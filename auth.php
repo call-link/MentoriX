77f09539e1d89c0647692e5f546f56f609d3e7e2
Hado <132592564+call-link@users.noreply.github.com>
2025-08-01 16:44:11 +0330

    completed

--- auth.php
+++ auth.php
@@ -7,17 +7,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST["name"] ?? '';
     $password = $_POST["pass"] ?? '';
 
-    $stmt = $conn->prepare("SELECT * FROM users WHERE name = ?");
-    $stmt->execute([$username]);
-    $user = $stmt->fetch();
-    var_dump($user);
-    // if ($user && $user['pass'] === $password) {
-    //     $_SESSION['name'] = $username;
-    //     header("Location: index.html");
-    //     exit;
-    // } else {
-    //     echo "<script>alert('Login failed. Try again.'); window.location.href = './views/auth/index-tpl.html';</script>";
-    //     exit;
-    // }
+    $sql = "SELECT * FROM `users` WHERE name = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username); 
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+    if ($username == $rows[0]['name'] && $password == $rows[0]['pass']) {
+        $_SESSION['name'] = $username;
+        header("Location: index.html");
+        exit;
+    } else {
+        echo "<script>alert('Login failed. Try again.'); window.location.href = './views/auth/index-tpl.html';</script>";
+        exit;
+    }
 }
-?>
